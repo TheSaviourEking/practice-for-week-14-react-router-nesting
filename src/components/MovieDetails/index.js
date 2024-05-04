ff9797901f58aac94c62370166517970f5c26e38
TheSaviourEking <saviour.eking@outlook.com>
2024-05-04 14:05:06 +0100

    chore: implement all practice problems

--- src/components/MovieDetails/index.js
+++ src/components/MovieDetails/index.js
@@ -1,7 +1,14 @@
-function MovieDetails() {
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+
+function MovieDetails({ movies }) {
+  const { movieId } = useParams();
+  const { title, description } = movies.find(movie => movie.id === parseInt(movieId));
+  
   return (
     <div className='comp purple'>
       <h1>MovieDetails Component</h1>
+      <h1>{title}</h1>
+      <p>{description}</p>
     </div>
   );
 }
--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -1,8 +1,26 @@
-function Movies() {
+import { NavLink, Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
+import MovieDetails from "../MovieDetails";
+
+function Movies({ movies }) {
   return (
     <div className='comp orange'>
       <h1>Movies Component</h1>
-    </div>
+      <nav>
+        <ul>
+          {movies.map(movie => {
+            return (<li key={movie.id}>
+              <NavLink to={`/movies/${movie.id}`}>{movie.id}</NavLink>
+            </li>
+            )
+          })}
+        </ul>
+      </nav>
+      <Switch>
+        <Route path='/movies/:movieId'>
+          <MovieDetails movies={movies} />
+        </Route>
+      </Switch>
+    </div >
   );
 }
 
